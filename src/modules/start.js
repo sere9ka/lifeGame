ebfe196ced6488e6169b69b5c7dcdaff3798c088
sere9ka <s.kovylov@zhiet.ru>
2022-02-08 11:19:59 +0300

    создание поля

--- src/modules/start.js
+++ src/modules/start.js
@@ -1,5 +1,41 @@
 const start = () => {
-    console.log('start');
+    const body = document.querySelector('body')
+    const blockForGame = body.querySelector('.main__block-game')
+    const arrWH = {}
+
+    const renderGame = arrWH => {
+        const blockForGameWidth = blockForGame.offsetWidth
+        const blockForGameHeight = blockForGame.offsetHeight
+        const newBlockGame = document.createElement('div')
+        const blockWidth = blockForGameWidth / arrWH.width
+        const blockHeight = blockForGameHeight / arrWH.height
+
+        newBlockGame.classList.add('game__block')
+        newBlockGame.style.width = `${blockWidth}px`
+        newBlockGame.style.height = `${blockHeight}px`
+        blockForGame.append(newBlockGame)
+
+        // blockForGame
+        console.log(blockWidth);
+        console.log(blockHeight);
+    }
+
+    const startGame = () => {
+        const inputGroups = body.querySelectorAll('.main__group')
+        inputGroups.forEach((group) => {
+            const nameKey = group.querySelector('label').textContent
+            const value = group.querySelector('input').value
+            arrWH[nameKey] = value
+        })
+        
+        renderGame(arrWH)
+    }
+
+    body.addEventListener('click', (e) => {
+        if (e.target == body.querySelector('button')) {
+            startGame()
+        }
+    })
 }
 
 export default start
